37d27a129822834ae331f9d17e3c9b226cacb06e
kangyi <kangyi9@h-partners.com>
2025-12-01 19:48:41 +0800

    feat: CHIR lib and plugin are independent
    Signed-off-by: kangyi <kangyi9@h-partners.com>

--- plugin/cmd.sh
+++ plugin/cmd.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 echo "start to build CHIR lib ..."
-cjc ../src/stdx/CHIR/lib/*.cj --output-type=dylib -g -Woff all -o libCHIRlib.so
+cjc ../src/stdx/CHIR/lib/*.cj --output-type=dylib -g -Woff all -o libCHIR.lib.so
 
-echo "start to build CHIR entry ..."
-cjc ../src/stdx/CHIR/pluginEntry/*.cj --output-type=dylib -g -o libCHIRentry.so
+echo "start to build CHIR serialize ..."
+cjc ../src/stdx/CHIR/serialize/*.cj --output-type=dylib -g -Woff all -L . -lCHIR.lib -o libCHIR.serialize.so
 
-echo "start to build CHIR macro ..."
-cjc ../src/stdx/CHIR/pluginMacro/*.cj --compile-macro -g
+echo "start to build plugin ..."
+cjc ../src/stdx/plugin/*.cj --output-type=dylib -g -Woff all -L . -lCHIR.lib -o libplugin.so
+
+echo "start to build plugin register ..."
+cjc ../src/stdx/plugin/register/*.cj --compile-macro -g -L . -lplugin -Woff all
+
+echo "start to build plugin execute ..."
+cjc ../src/stdx/plugin/execute/*.cj --output-type=dylib -g -Woff all -L . -lCHIR.serialize -lplugin -o libplugin.execute.so
 
 echo "start to build plugin1 ..."
-cjc plugin1.cj -L . -lCHIRlib -lCHIRentry -l-macro_stdx.CHIR.pluginMacro --output-type=dylib -o libplugin1.so
+cjc plugin1.cj -Woff all -L . -lplugin.execute -l-macro_stdx.plugin.register --output-type=dylib -o libplugin1.so
 
 echo "start to build plugin2 ..."
-cjc plugin2.cj -L . -lCHIRlib -lCHIRentry -l-macro_stdx.CHIR.pluginMacro --output-type=dylib -o libplugin2.so
+cjc plugin2.cj -Woff all -L . -lplugin.execute -l-macro_stdx.plugin.register --output-type=dylib -o libplugin2.so
 
 echo "start to build test case..."
 cjc test.cj --plugin=libplugin1.so --plugin=libplugin2.so
