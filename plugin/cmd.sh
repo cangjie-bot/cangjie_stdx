9d97a2ed96552bfd791a626b64a7166d34c0e80b
kangyi <kangyi9@h-partners.com>
2025-12-10 15:48:42 +0800

    fix: add hash code
    Signed-off-by: kangyi <kangyi9@h-partners.com>

--- plugin/cmd.sh
+++ plugin/cmd.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo "start to build CHIR lib ..."
-cjc ../src/stdx/CHIR/lib/*.cj --output-type=dylib -g -Woff all -o libCHIR.lib.so
+cjc ../src/stdx/CHIR/lib/*.cj --output-type=dylib -g -Woff all --no-sub-pkg -o libCHIR.lib.so
 
 echo "start to build CHIR serialize ..."
-cjc ../src/stdx/CHIR/serialize/*.cj --output-type=dylib -g -Woff all -L . -lCHIR.lib -o libCHIR.serialize.so
+cjc ../src/stdx/CHIR/serialize/*.cj --output-type=dylib -g -Woff all --no-sub-pkg -L . -lCHIR.lib -o libCHIR.serialize.so
 
 echo "start to build plugin ..."
 cjc ../src/stdx/plugin/*.cj --output-type=dylib -g -Woff all -L . -lCHIR.lib -o libplugin.so
 
 echo "start to build plugin register ..."
-cjc ../src/stdx/plugin/register/*.cj --compile-macro -g -L . -lplugin -Woff all
+cjc ../src/stdx/plugin/register/*.cj --compile-macro -g --no-sub-pkg -L . -lplugin -Woff all
 
 echo "start to build plugin execute ..."
-cjc ../src/stdx/plugin/execute/*.cj --output-type=dylib -g -Woff all -L . -lCHIR.serialize -lplugin -o libplugin.execute.so
+cjc ../src/stdx/plugin/execute/*.cj --output-type=dylib -g -Woff all --no-sub-pkg -L . -lCHIR.serialize -lplugin -o libplugin.execute.so
 
 echo "start to build plugin1 ..."
 cjc plugin1.cj -Woff all -L . -lplugin.execute -l-macro_stdx.plugin.register --output-type=dylib -o libplugin1.so
